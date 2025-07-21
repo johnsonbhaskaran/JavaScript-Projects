1b2bd981c6619bca82ed3934a2dd6a0b3724acd1
JohnsonB <johnsonb@live.in>
2025-07-21 22:09:08 +0530

    Update script.js
    JS updated

--- Responsive_Number_counting_Animation/script.js
+++ Responsive_Number_counting_Animation/script.js
@@ -1,1 +1,17 @@
 console.log("Responsive Number Counting Animation");
+
+let valueDisplay = document.querySelectorAll(".num");
+let interval = 2000;
+
+valueDisplay.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+
+  let duration = Math.floor(interval / endValue);
+
+  let counter = setInterval(() => {
+    startValue += 1;
+    valueDisplay.textContent = startValue;
+    if (startValue === endValue) clearInterval(counter);
+  }, duration);
+});
