39bde6c8fa63837283d34a793b4a484d349c9e6e
Johnson Bhaskaran <johnsonb@live.in>
2025-07-23 08:37:38 +0530

    First attempt for color flipper logic building

--- Color-Flipper/app.js
+++ Color-Flipper/app.js
@@ -1,1 +1,16 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const colorEl = document.querySelector(".color");
+const btnEl = document.getElementById("btn");
+const bodyEl = document.querySelector("body");
+
+let i = 0;
+btnEl.addEventListener("click", () => {
+  colorEl.textContent = colors[i++];
+  bodyEl.style.background = colorEl.textContent;
+  console.log(i);
+  console.log(colorEl.textContent);
+  if (i >= colors.length) {
+    i = 0;
+  }
+});
