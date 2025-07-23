9820f3fcaf5f6259196e802fd56dcaa51def67ba
Johnson Bhaskaran <johnsonb@live.in>
2025-07-23 16:01:06 +0530

    function refactored using random number generation

--- Color-Flipper/app.js
+++ Color-Flipper/app.js
@@ -2,15 +2,19 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
 const colorEl = document.querySelector(".color");
 const btnEl = document.getElementById("btn");
-const bodyEl = document.querySelector("body");
+const bodyEl = document.body;
 
-let i = 0;
+// let i = 0;
+// btnEl.addEventListener("click", () => {
+//   colorEl.textContent = colors[i++];
+//   bodyEl.style.background = colorEl.textContent;
+//   if (i >= colors.length) {
+//     i = 0;
+//   }
+// });
+
 btnEl.addEventListener("click", () => {
-  colorEl.textContent = colors[i++];
-  bodyEl.style.background = colorEl.textContent;
-  console.log(i);
-  console.log(colorEl.textContent);
-  if (i >= colors.length) {
-    i = 0;
-  }
+  // * Get random number between 0 - 3 colors[]
+  const random = Math.floor(Math.random() * 4);
+  bodyEl.style.backgroundColor = colors[random];
 });
