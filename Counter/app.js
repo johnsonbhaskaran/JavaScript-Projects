992124c676b1f3f4c6c34db91fbf252de0a03c4b
Johnson Bhaskaran <johnsonb@live.in>
2025-07-23 17:10:02 +0530

    added color value based on counter value

--- Counter/app.js
+++ Counter/app.js
@@ -13,6 +13,10 @@ btns.forEach((btn) => {
       count++;
     } else count = 0;
 
+    if (count > 0) value.style.color = "green";
+    if (count < 0) value.style.color = "red";
+    if (count === 0) value.style.color = "#222";
+
     value.textContent = count;
   });
 });
