c1b3139d6b795a1371bf444d19faca78844ee622
Johnson Bhaskaran <johnsonb@live.in>
2025-07-24 21:50:56 +0530

    First attempt

--- NavBar-Toggle/app.js
+++ NavBar-Toggle/app.js
@@ -3,3 +3,14 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const toggleButton = document.querySelector(".nav-toggle");
+const linksClass = document.querySelector(".links");
+
+// console.log(toggleButton);
+
+toggleButton.addEventListener("click", () => {
+  linksClass.classList.contains("active")
+    ? linksClass.classList.remove("active")
+    : linksClass.classList.add("active");
+});
