eca66b11df3a528ed169c5f18d5d2da031ff5397
Johnson Bhaskaran <johnsonb@live.in>
2025-07-25 22:31:39 +0530

    Partially implemented

--- Accordion-FAQs-Questions/app.js
+++ Accordion-FAQs-Questions/app.js
@@ -1,18 +1,18 @@
 //using selectors inside the element
 // traversing the dom
 
-const question = document.querySelector(".question");
+const questions = document.querySelectorAll(".question");
 const plusIcon = document.querySelector(".plus-icon");
 const minusIcon = document.querySelector(".minus-icon");
 
-question.addEventListener("click", () => {
-  question.classList.toggle("show-text");
-});
-
-plusIcon.addEventListener("click", () => {
-  question.classList.add(".show-text");
-});
-
-question.addEventListener("click", () => {
-  question.classList.remove(".show-text");
+questions.forEach((question) => {
+  question.addEventListener("click", () => {
+    question.classList.toggle("show-text");
+  });
+  plusIcon.addEventListener("click", () => {
+    question.classList.add("show-text");
+  });
+  minusIcon.addEventListener("click", () => {
+    question.classList.remove("show-text");
+  });
 });
