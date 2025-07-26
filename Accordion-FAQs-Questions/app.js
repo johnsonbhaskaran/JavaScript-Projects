87bcd40f01c47efa4589251d8ad9fbea180d4783
Johnson Bhaskaran <johnsonb@live.in>
2025-07-26 08:44:39 +0530

    First attempt full functionality completed implementing

--- Accordion-FAQs-Questions/app.js
+++ Accordion-FAQs-Questions/app.js
@@ -2,17 +2,22 @@
 // traversing the dom
 
 const questions = document.querySelectorAll(".question");
-const plusIcon = document.querySelector(".plus-icon");
-const minusIcon = document.querySelector(".minus-icon");
+// const plusIcon = document.querySelector(".plus-icon");
+// const minusIcon = document.querySelector(".minus-icon");
 
 questions.forEach((question) => {
-  question.addEventListener("click", () => {
-    question.classList.toggle("show-text");
-  });
-  plusIcon.addEventListener("click", () => {
-    question.classList.add("show-text");
-  });
-  minusIcon.addEventListener("click", () => {
-    question.classList.remove("show-text");
+  question.addEventListener("click", (e) => {
+    let plusIcon = e.currentTarget.querySelector(".plus-icon");
+    let minusIcon = e.currentTarget.querySelector(".minus-icon");
+
+    plusIcon.addEventListener("click", (e) => {
+      e.currentTarget.parentNode.parentNode.parentNode.parentNode.classList.add("show-text");
+      console.log(e.currentTarget);
+    });
+
+    minusIcon.addEventListener("click", (e) => {
+      e.currentTarget.parentNode.parentNode.parentNode.parentNode.classList.remove("show-text");
+      console.log(e.currentTarget);
+    });
   });
 });
