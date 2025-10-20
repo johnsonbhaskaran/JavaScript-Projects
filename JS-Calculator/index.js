df9b50186b25a246dd94704826f4c2bd0879de49
Johnson Bhaskaran <johnsonb@live.in>
2025-10-20 07:34:00 +0530

    Done: completed implementing the functions for addToInput, clearInput, backspace, evaluateVal

--- JS-Calculator/index.js
+++ JS-Calculator/index.js
@@ -1,1 +1,17 @@
-document.body.addEventListener("onload", document.body.append("HI"));
+let input = document.querySelector("input");
+
+function addToInput(val) {
+  input.value += val;
+}
+
+function clearInput() {
+  input.value = "";
+}
+
+function backspace() {
+  input.value = input.value.slice(0, -1);
+}
+
+function evaluateVal() {
+  input.value = eval(input.value);
+}
