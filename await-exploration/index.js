9c46a654719197f36b01d182fe26fe5777d21826
Johnson Bhaskaran <johnsonb@live.in>
2025-10-23 12:47:10 +0530

    Done: await with forEach

--- await-exploration/index.js
+++ await-exploration/index.js
@@ -1,9 +1,14 @@
-async function fetchUserProfiles() {
-  const userIds = [1, 2, 3, 4, 5];
+const userIds = [1, 2, 3, 4, 5];
 
+/* -----------------------------------------------------------------/
+                    * await - with forEach() *
+/------------------------------------------------------------------*/
+
+async function fetchUserProfiles() {
   userIds.forEach(async (id) => {
     const profile = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     const data = await profile.json();
+
     console.log(`User ${id}:`, data.name);
   });
 
